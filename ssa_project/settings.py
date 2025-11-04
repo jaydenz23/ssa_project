c91d21ee6c2ad52ca18d562a511c9299e0f4895c
Jayden Zhou <jaydenzhou@jaydens-macbook.local>
2025-11-04 14:21:09 +1100

    Group Join Requests

--- ssa_project/settings.py
+++ ssa_project/settings.py
@@ -55,3 +55,20 @@ USE_I18N = True
 USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'chipin.context_processors.user_profile',
+            ],
+        },
+    },
+]
